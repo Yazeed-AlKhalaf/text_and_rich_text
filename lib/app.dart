21ad4668b4b125a6ed9b7c2486162d75bd48e462
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-03-17 19:50:35 +0300

    🌟 Starter Project

--- lib/app.dart
+++ lib/app.dart
@@ -9,14 +9,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool showRichText = false;
+
   @override
   Widget build(BuildContext context) {
+    Widget _buildText() {
+      return Text("I love Flutter so much 💙");
+    }
+
+    Widget _buildRichText() {
+      return Text("I love Flutter so much 💙");
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text(
-          "I love Flutter so much 💙",
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: showRichText ? _buildRichText() : _buildText(),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                showRichText = !showRichText;
+              });
+            },
+            child: Text(
+              showRichText ? "Show Text" : "Show Rich Text",
+            ),
+          ),
+        ],
       ),
     );
   }
